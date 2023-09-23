b13af49c322399c30ed15d831e38c4ca0b702b99
Himangshu Mondal <mailtoherock@gmail.com>
2023-09-23 11:39:25 +0530

    react useState hook check

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
 function App() {
-  return React.createElement(
-    'div',
-    { className: 'test' },
-    React.createElement('h1', {}, 'hello')
+  /* return of useState is a array
+    1. state variable
+    2. state variable updater function
+  */
+  const [value, setValue] = React.useState(0);
+
+  const handleClick = () => {
+    setValue(value + 1);
+  };
+  return (
+    <>
+      <h2>value : {value}</h2>
+      <button onClick={handleClick}>click</button>
+    </>
   );
 }
 
